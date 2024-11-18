908dd07a20ad6382963000117e7e44843ba461fb
Christopher Bartling <chris.bartling@gmail.com>
2024-11-18 09:28:36 -0600

    tests: adding unit tests

--- toxiproxy-clone/src/toxics/corrupt.rs
+++ toxiproxy-clone/src/toxics/corrupt.rs
@@ -1,5 +1,5 @@
-use rand::Rng;
 use crate::toxic::{Toxic, ToxicConfig};
+use rand::Rng;
 
 pub struct CorruptToxic {
     pub probability: f64,
@@ -26,6 +26,51 @@ impl Toxic for CorruptToxic {
     fn get_config(&self) -> ToxicConfig {
         ToxicConfig::Corrupt {
             probability: self.probability,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn corrupt_toxic_modifies_upstream_with_probability() {
+        let mut data = vec![1, 2, 3, 4];
+        let toxic = CorruptToxic { probability: 1.0 };
+        toxic.modify_upstream(&mut data);
+        assert_ne!(data[0], 1);
+    }
+
+    #[test]
+    fn corrupt_toxic_does_not_modify_upstream_with_zero_probability() {
+        let mut data = vec![1, 2, 3, 4];
+        let toxic = CorruptToxic { probability: 0.0 };
+        toxic.modify_upstream(&mut data);
+        assert_eq!(data[0], 1);
+    }
+
+    #[test]
+    fn corrupt_toxic_modifies_downstream() {
+        let mut data = vec![1, 2, 3, 4];
+        let toxic = CorruptToxic { probability: 1.0 };
+        toxic.modify_downstream(&mut data);
+        assert_ne!(data[0], 1);
+    }
+
+    #[test]
+    fn corrupt_toxic_get_type_returns_correct_type() {
+        let toxic = CorruptToxic { probability: 0.5 };
+        assert_eq!(toxic.get_type(), "corrupt");
+    }
+
+    #[test]
+    fn corrupt_toxic_get_config_returns_correct_config() {
+        let toxic = CorruptToxic { probability: 0.5 };
+        if let ToxicConfig::Corrupt { probability } = toxic.get_config() {
+            assert_eq!(probability, 0.5);
+        } else {
+            panic!("Expected ToxicConfig::Corrupt");
         }
     }
 }
