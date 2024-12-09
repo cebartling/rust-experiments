3c13746dcedf22590a914e970580aea09bee2e64
Christopher Bartling <chris.bartling@gmail.com>
2024-12-08 22:43:31 -0600

    feat: creating skeleton app

---
+++ leptos-pomodoro/src/main.rs
@@ -0,0 +1,33 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <main>
+            <h1>"Welcome to Leptos!"</h1>
+            <Counter/>
+        </main>
+    }
+}
+
+#[component]
+fn Counter() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <div>
+            <button
+                on:click=move |_| set_count.update(|count| *count += 1)
+            >
+                "Count: " {count}
+            </button>
+        </div>
+    }
+}
+
+fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(App);
+}
