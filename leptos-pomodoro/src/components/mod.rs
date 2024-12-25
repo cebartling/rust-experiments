04ad74c552a9507b41cc8864e9793b9346467723
Christopher Bartling <chris.bartling@gmail.com>
2024-12-25 15:14:56 -0600

    chore: reformatting source

--- leptos-pomodoro/src/components/mod.rs
+++ leptos-pomodoro/src/components/mod.rs
@@ -1,6 +1,6 @@
-pub(crate) mod pomodoro_timer;
 pub(crate) mod completed_pomodoros;
 pub(crate) mod no_completed_pomodoros;
+pub(crate) mod pomodoro_timer;
 pub(crate) mod tomato_icon;
 
 mod tomato_icon_tests;
