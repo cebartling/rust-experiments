aeb33773cb0a372c6b589cc0340e6500e3069c44
Christopher Bartling <chris.bartling@gmail.com>
2024-12-25 15:13:58 -0600

    chore: removing empty icon

--- leptos-pomodoro/src/components/empty_icon.rs
+++
@@ -1,9 +0,0 @@
-use leptos::prelude::*;
-
-#[component]
-pub fn EmptyIcon() -> impl IntoView {
-    view! {
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 h-6 inline-block">
-        </svg>
-    }
-}
--- leptos-pomodoro/src/components/mod.rs
+++ leptos-pomodoro/src/components/mod.rs
@@ -2,6 +2,5 @@ pub(crate) mod pomodoro_timer;
 pub(crate) mod completed_pomodoros;
 pub(crate) mod no_completed_pomodoros;
 pub(crate) mod tomato_icon;
-pub(crate) mod empty_icon;
 
 mod tomato_icon_tests;
