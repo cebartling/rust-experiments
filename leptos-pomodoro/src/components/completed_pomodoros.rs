2532e95cb35581be0da0c715f0d6500b360d7ba7
Christopher Bartling <chris.bartling@gmail.com>
2024-12-25 21:01:19 -0600

    chore: formatting with leptosfmt

--- leptos-pomodoro/src/components/completed_pomodoros.rs
+++ leptos-pomodoro/src/components/completed_pomodoros.rs
@@ -11,11 +11,7 @@ pub fn CompletedPomodoros(
         <div class="mb-4">
             <h2 class="text-xl font-semibold mb-2">"Completed Pomodoros"</h2>
             <div class="flex flex-wrap justify-center gap-2 transition-opacity ease-in-out delay-500">
-                {move || {
-                    (0..count.get())
-                        .map(|_| view! { <TomatoIcon/> })
-                        .collect::<Vec<_>>()
-                }}
+                {move || { (0..count.get()).map(|_| view! { <TomatoIcon /> }).collect::<Vec<_>>() }}
             </div>
         </div>
     }
