6c7bef84299ba50e11990b896181a31c3ac6a90d
Christopher Bartling <chris.bartling@gmail.com>
2024-12-28 00:21:07 -0600

    feat: running working app

---
+++ leptos-shopping-cart/src/components/app.rs
@@ -0,0 +1,13 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div class="bg-white p-8 rounded-lg shadow-lg text-center">
+                <h1 class="text-4xl font-bold mb-8">"Leptos Shopping Cart"</h1>
+
+            </div>
+        </div>
+    }
+}
--- leptos-shopping-cart/src/main.rs
+++ leptos-shopping-cart/src/main.rs
@@ -1,3 +1,11 @@
+mod components;
+
+use leptos::prelude::*;
+use crate::components::app::App;
+
 fn main() {
-    println!("Hello, world!");
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(App);
 }
