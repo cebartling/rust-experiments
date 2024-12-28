efdcd4bac52a6735aaa8f6ccf8b3f64e1393a854
Christopher Bartling <chris.bartling@gmail.com>
2024-12-28 11:53:57 -0600

    chore: reformatting leptos view code

--- leptos-shopping-cart/src/components/product_detail_view.rs
+++ leptos-shopping-cart/src/components/product_detail_view.rs
@@ -4,7 +4,7 @@ use leptos::prelude::*;
 pub fn ProductDetailView() -> impl IntoView {
     view! {
         <div class="flex flex-col grow">
-                <h3 class="text-2xl font-bold mb-8">"Product Detail"</h3>
+            <h3 class="text-2xl font-bold mb-8">"Product Detail"</h3>
         </div>
     }
 }
